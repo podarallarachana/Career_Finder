77481877283f0b41350e1010a840fb6f489a1f79
iracoon <podarallarachana@ufl.edu>
2020-03-12 07:03:45 -0500

    Added frontend dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
