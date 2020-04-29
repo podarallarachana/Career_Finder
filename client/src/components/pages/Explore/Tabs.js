14f1d078b68388b69e95aadda3e41d4feaf2c41f
iracoon <podarallarachana@ufl.edu>
2020-04-29 14:57:02 -0500

    Divide learning module cards into tabs

--- client/src/components/pages/Explore/Tabs.js
+++ client/src/components/pages/Explore/Tabs.js
@@ -5,6 +5,16 @@ import LearningModulesHeader from "./LearningModules/LearningModulesHeader";
 import ToolsTech from "./LearningModules/ToolsTech";
 import CardColumns from "react-bootstrap/CardColumns";
 import Spinner from "react-bootstrap/Spinner";
+import Abilities from "./LearningModules/Abilities";
+import Knowledge from "./LearningModules/Knowledge";
+import Skills from "./LearningModules/Skills";
+import Related from "./LearningModules/Related";
+import Activities from "./LearningModules/Activities";
+import Education from "./LearningModules/Education";
+import Wages from "./LearningModules/Wages";
+import Interests from "./LearningModules/Interests";
+import Outlook from "./LearningModules/Outlook";
+import Tasks from "./LearningModules/Tasks";
 
 const Tabs = (props) => {
   const [activeTab, setActiveTab] = useState("general");
@@ -26,7 +36,7 @@ const Tabs = (props) => {
         variant="pills"
         defaultActiveKey={activeTab}
         onSelect={handleSelect}
-        style={{ borderRadius: "0px" }}
+        style={{ borderRadius: "0px", backgroundColor: "white" }}
       >
         <Nav.Item>
           <Nav.Link eventKey="general" style={{ borderRadius: "0px" }}>
@@ -34,8 +44,8 @@ const Tabs = (props) => {
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="fit" style={{ borderRadius: "0px" }}>
-            Fit
+          <Nav.Link eventKey="knowledge" style={{ borderRadius: "0px" }}>
+            Knowledge
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
@@ -58,17 +68,143 @@ const Tabs = (props) => {
           ) : null}
           {props.data !== null && props.data !== undefined ? (
             <CardColumns>
-              <LearningModules
-                updateActives={props.updateActives}
-                data={props.data}
-              />
+              {props.data.OccupationDetail[0].hasOwnProperty("Wages") &&
+              props.data.OccupationDetail[0].Wages.NationalWagesList.length !==
+                0 &&
+              props.data.OccupationDetail[0].Wages.NationalWagesList !==
+                undefined &&
+              props.data.OccupationDetail[0].Wages.NationalWagesList !==
+                null ? (
+                <Wages data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "RelatedOnetTitles"
+              ) &&
+              props.data.OccupationDetail[0].RelatedOnetTitles !== undefined &&
+              props.data.OccupationDetail[0].RelatedOnetTitles !== null &&
+              Object.keys(
+                props.data.OccupationDetail[0].RelatedOnetTitles
+              )[0] !== undefined ? (
+                <Related
+                  updateActives={props.updateActives}
+                  data={props.data}
+                />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "EducationTraining"
+              ) &&
+              props.data.OccupationDetail[0].EducationTraining.EducationType
+                .length !== 0 &&
+              props.data.OccupationDetail[0].EducationTraining.EducationType !==
+                undefined &&
+              props.data.OccupationDetail[0].EducationTraining.EducationType !==
+                null ? (
+                <Education data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty("Projections") &&
+              props.data.OccupationDetail[0].Projections.Projections.length !==
+                0 &&
+              props.data.OccupationDetail[0].Projections.Projections !==
+                undefined &&
+              props.data.OccupationDetail[0].Projections.Projections !==
+                null ? (
+                <Outlook data={props.data} />
+              ) : null}
+            </CardColumns>
+          ) : null}
+        </div>
+      ) : null}
+
+      {activeTab === "knowledge" ? (
+        <div className="learningModules">
+          {props.data === undefined ? (
+            <div className="learningModules">
+              <div className="row justify-content-center">
+                <Spinner animation="grow" />
+              </div>
+            </div>
+          ) : null}
+          {props.data === null ? (
+            <div className="learningModules">No</div>
+          ) : null}
+          {props.data !== null && props.data !== undefined ? (
+            <CardColumns>
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "SkillsDataList"
+              ) &&
+              props.data.OccupationDetail[0].SkillsDataList.length !== 0 &&
+              props.data.OccupationDetail[0].SkillsDataList !== undefined &&
+              props.data.OccupationDetail[0].SkillsDataList !== null ? (
+                <Skills data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "KnowledgeDataList"
+              ) &&
+              props.data.OccupationDetail[0].KnowledgeDataList.length !== 0 &&
+              props.data.OccupationDetail[0].KnowledgeDataList !== undefined &&
+              props.data.OccupationDetail[0].KnowledgeDataList !== null ? (
+                <Knowledge data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "AbilityDataList"
+              ) &&
+              props.data.OccupationDetail[0].AbilityDataList.length !== 0 &&
+              props.data.OccupationDetail[0].AbilityDataList !== undefined &&
+              props.data.OccupationDetail[0].AbilityDataList !== null ? (
+                <Abilities data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty(
+                "InterestDataList"
+              ) &&
+              props.data.OccupationDetail[0].InterestDataList.length !== 0 &&
+              props.data.OccupationDetail[0].InterestDataList !== undefined &&
+              props.data.OccupationDetail[0].InterestDataList !== null ? (
+                <Interests data={props.data} />
+              ) : null}
+            </CardColumns>
+          ) : null}
+        </div>
+      ) : null}
+
+      {activeTab === "worklife" ? (
+        <div className="learningModules">
+          {props.data === undefined ? (
+            <div className="learningModules">
+              <div className="row justify-content-center">
+                <Spinner animation="grow" />
+              </div>
+            </div>
+          ) : null}
+          {props.data === null ? (
+            <div className="learningModules">No</div>
+          ) : null}
+          {props.data !== null && props.data !== undefined ? (
+            <CardColumns>
+              {props.data.OccupationDetail[0].hasOwnProperty("Dwas") &&
+              props.data.OccupationDetail[0].Dwas.length !== 0 &&
+              props.data.OccupationDetail[0].Dwas !== undefined &&
+              props.data.OccupationDetail[0].Dwas !== null ? (
+                <Activities data={props.data} />
+              ) : null}
+
+              {props.data.OccupationDetail[0].hasOwnProperty("Tasks") &&
+              props.data.OccupationDetail[0].Tasks.length !== 0 &&
+              props.data.OccupationDetail[0].Tasks !== undefined &&
+              props.data.OccupationDetail[0].Tasks !== null ? (
+                <Tasks data={props.data} />
+              ) : null}
+
               <ToolsTech toolsData={props.toolsData} />
             </CardColumns>
           ) : null}
         </div>
-      ) : (
-        <p>hi</p>
-      )}
+      ) : null}
     </Fragment>
   );
 };
